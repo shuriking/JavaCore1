5fc27d1a0a0584fcd8ff9482cd1fd307216051e7
shuriking <shura.kravchenko.1992@gmail.com>
2021-10-28 17:02:38 +0300

    Lesson1

---
+++ src/main/java/Lesson1/HomeWorkApp.java
@@ -0,0 +1,50 @@
+package Lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 3;
+        int b = 5;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отррицательная");
+        }
+    }
+    public static void printColor(){
+        int value = -200;
+
+        if (value <=0){
+            System.out.println("Красный");
+        }else if (value > 1 && value < 100){
+            System.out.println("Желтый");
+        }else if(value>100){
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers(){
+        int a = 35;
+        int b = 43;
+
+        if(a>=b){
+            System.out.println("a>=b");
+        }else{
+            System.out.println("a<b");
+        }
+    }
+}
+
